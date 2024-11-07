954500155a461820d200e6d8900c73411d6ff948
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-07 22:57:24 +0300

    feat: add statuses to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,13 @@
-// @ts-check
-
 import welcome from './welcome.js';
 import users from './users.js';
 import session from './session.js';
+import statuses from './statuses.js';
 
 const routes = [
   welcome,
   users,
   session,
+  statuses,
 ];
 
 export default (app) => routes.forEach((f) => f(app));
