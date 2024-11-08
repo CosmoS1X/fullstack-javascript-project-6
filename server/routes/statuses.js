6702524224598bf662213309e40b281a5acdc43c
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-08 22:28:23 +0300

    feat: add the rest routes for statuses
    GET /statuses/:id/edit
    POST /statuses
    PATCH /statuses/:id
    DELETE /statuses/:id

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -3,11 +3,10 @@ import statusController from '../controllers/statusController.js';
 export default (app) => {
   const controller = statusController(app);
 
-  app.get('/statuses', { name: 'statuses' }, controller.getList);
-  app.get('/statuses/new', { name: 'newStatus' }, controller.getCreateForm);
-
-  // GET /statuses/:id/edit - страница редактирования статуса
-  // POST /statuses - создание нового статуса
-  // PATCH /statuses/:id - обновление статуса
-  // DELETE /statuses/:id - удаление статуса
+  app.get('/statuses', { name: 'statuses', preValidation: app.authenticate }, controller.getList);
+  app.get('/statuses/new', { name: 'newStatus', preValidation: app.authenticate }, controller.getCreateForm);
+  app.get('/statuses/:id/edit', { name: 'editStatus', preValidation: app.authenticate }, controller.getUpdateForm);
+  app.post('/statuses', { preValidation: app.authenticate }, controller.create);
+  app.patch('/statuses/:id', { preValidation: app.authenticate }, controller.update);
+  app.delete('/statuses/:id', { preValidation: app.authenticate }, controller.delete);
 };
