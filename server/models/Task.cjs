0ce81a40978cbb2e4745ee3a558f76b2f844b8ce
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-15 20:02:11 +0300

    fix: update validation schema

--- server/models/Task.cjs
+++ server/models/Task.cjs
@@ -15,9 +15,9 @@ module.exports = class Task extends BaseModel {
         id: { type: 'integer' },
         name: { type: 'string', minLength: 1 },
         description: { type: 'string' },
-        statusId: { type: 'integer' },
-        creatorId: { type: 'integer' },
-        executorId: { type: 'integer' },
+        statusId: { type: 'integer', minimum: 1 },
+        creatorId: { type: 'integer', minimum: 1 },
+        executorId: { type: ['integer', 'null'], default: null },
       },
     };
   }
