c9e3a30f2c4390525c34dac3ad288795d6f6ed17
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-15 20:21:55 +0300

    feat: add tasks to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@ import welcome from './welcome.js';
 import users from './users.js';
 import session from './session.js';
 import taskStatuses from './taskStatuses.js';
+import tasks from './tasks.js';
 
 const routes = [
   welcome,
   users,
   session,
   taskStatuses,
+  tasks,
 ];
 
 export default (app) => routes.forEach((f) => f(app));
