5e172765e1a9bc79fdcb65b44d70d849e53f623d
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-16 23:52:01 +0300

    feat: update task controller
    + show
    + renderUpdateForm
    + update

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -5,10 +5,12 @@ export default (app) => {
 
   return {
     getList: async (req, reply) => {
-      const tasks = await models.task.query()
+      const tasks = await models.task
+        .query()
         .withGraphFetched('status')
         .withGraphFetched('creator')
-        .withGraphFetched('executor');
+        .withGraphFetched('executor')
+        .orderBy('createdAt', 'desc');
 
       reply.render('tasks/index', { tasks });
 
@@ -24,12 +26,7 @@ export default (app) => {
       return reply;
     },
     create: async (req, reply) => {
-      const {
-        name,
-        description,
-        statusId,
-        executorId,
-      } = req.body.data;
+      const { name, description, statusId, executorId } = req.body.data;
 
       const preparedData = {
         name,
@@ -62,6 +59,65 @@ export default (app) => {
 
         req.flash('error', i18next.t('flash.tasks.create.error'));
         reply.render('tasks/new', data);
+      }
+
+      return reply;
+    },
+    show: async (req, reply) => {
+      const task = await models.task
+        .query()
+        .findById(req.params.id)
+        .withGraphFetched('status')
+        .withGraphFetched('creator')
+        .withGraphFetched('executor');
+
+      reply.render('tasks/show', { task });
+
+      return reply;
+    },
+    renderUpdateForm: async (req, reply) => {
+      const task = await models.task
+        .query()
+        .findById(req.params.id)
+        .withGraphFetched('status')
+        .withGraphFetched('creator')
+        .withGraphFetched('executor');
+
+      const users = await models.user.query();
+      const statuses = await models.taskStatus.query();
+
+      reply.render('tasks/edit', { task, users, statuses });
+
+      return reply;
+    },
+    update: async (req, reply) => {
+      const task = await models.task.query().findById(req.params.id);
+      const { name, description, statusId, executorId } = req.body.data;
+
+      const preparedData = {
+        name,
+        description,
+        statusId: Number(statusId) || 0,
+        executorId: Number(executorId) || null,
+      };
+
+      try {
+        await task.$query().patch(preparedData);
+
+        req.flash('info', i18next.t('flash.tasks.edit.success'));
+        reply.redirect(app.reverse('tasks'));
+      } catch (error) {
+        const users = await models.user.query();
+        const statuses = await models.taskStatus.query();
+        const data = {
+          task,
+          users,
+          statuses,
+          errors: error.data,
+        };
+
+        req.flash('error', i18next.t('flash.tasks.edit.error'));
+        reply.render('tasks/edit', data);
       }
 
       return reply;
