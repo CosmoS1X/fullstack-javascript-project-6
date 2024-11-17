cf1bd1e38cd6cff24200699f154149fc54c22fab
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-17 19:16:06 +0300

    fix: add pre validation for 'show task' route

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -5,7 +5,7 @@ export default (app) => {
 
   app.get('/tasks', { name: 'tasks', preValidation: app.authenticate }, controller.getList);
   app.get('/tasks/new', { name: 'newTask', preValidation: app.authenticate }, controller.renderCreateForm);
-  app.get('/tasks/:id', { name: 'task' }, controller.show);
+  app.get('/tasks/:id', { name: 'task', preValidation: app.authenticate }, controller.show);
   app.get('/tasks/:id/edit', { name: 'editTask', preValidation: app.authenticate }, controller.renderUpdateForm);
   app.post('/tasks', { preValidation: app.authenticate }, controller.create);
   app.patch('/tasks/:id', { preValidation: app.authenticate }, controller.update);
