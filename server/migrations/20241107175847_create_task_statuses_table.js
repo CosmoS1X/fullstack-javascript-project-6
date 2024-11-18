c1ad651a46310f19e928417d81ef2d5b9cdd9a4f
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-19 01:33:57 +0300

    feat: update migrations

--- server/migrations/20241107175847_create_task_statuses_table.js
+++ server/migrations/20241107175847_create_task_statuses_table.js
@@ -1,9 +1,8 @@
 export const up = (knex) => (
   knex.schema.createTable('task_statuses', (table) => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').notNullable().unique();
     table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 );
 
--- server/migrations/20241112134337_create_tasks_table.js
+++ server/migrations/20241112134337_create_tasks_table.js
@@ -1,14 +1,24 @@
-export const up = (knex) => (
+export const up = (knex) =>
   knex.schema.createTable('tasks', (table) => {
     table.increments('id').primary();
     table.string('name').notNullable();
-    table.string('description');
-    table.integer('status_id').notNullable().references('id').inTable('task_statuses');
-    table.integer('creator_id').notNullable().references('id').inTable('users');
-    table.integer('executor_id').references('id').inTable('users');
+    table.text('description');
+    table
+      .integer('status_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('task_statuses')
+      .onDelete('RESTRICT');
+    table
+      .integer('creator_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('RESTRICT');
+    table.integer('executor_id').unsigned().references('id').inTable('users').onDelete('RESTRICT');
     table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
-);
+  });
 
 export const down = (knex) => knex.schema.dropTable('tasks');
