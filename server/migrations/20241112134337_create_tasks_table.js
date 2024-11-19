01dd86cc43d7dd12adfc74161bd5990e0cc2254c
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-19 12:39:43 +0300

    refactor: delete unnecessary onDelete('RESTRICT')

--- server/migrations/20241112134337_create_tasks_table.js
+++ server/migrations/20241112134337_create_tasks_table.js
@@ -3,21 +3,9 @@ export const up = (knex) =>
     table.increments('id').primary();
     table.string('name').notNullable();
     table.text('description');
-    table
-      .integer('status_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('task_statuses')
-      .onDelete('RESTRICT');
-    table
-      .integer('creator_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onDelete('RESTRICT');
-    table.integer('executor_id').unsigned().references('id').inTable('users').onDelete('RESTRICT');
+    table.integer('status_id').unsigned().notNullable().references('id').inTable('task_statuses');
+    table.integer('creator_id').unsigned().notNullable().references('id').inTable('users');
+    table.integer('executor_id').unsigned().references('id').inTable('users');
     table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 
