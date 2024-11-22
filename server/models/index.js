c57f6665b778d40df883d804762e22e7d840721c
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-22 22:27:36 +0300

    feat: add 'TaskLabel' entity to models

--- server/models/index.js
+++ server/models/index.js
@@ -2,10 +2,12 @@ import User from './User.cjs';
 import TaskStatus from './TaskStatus.cjs';
 import Task from './Task.cjs';
 import Label from './Label.cjs';
+import TaskLabel from './TaskLabel.cjs';
 
 export default [
   User,
   TaskStatus,
   Task,
   Label,
+  TaskLabel,
 ];
