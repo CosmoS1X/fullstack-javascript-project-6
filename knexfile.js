13d0203893fcd9f716a3de80e0b577aeb77efa89
Konstantin Korovin <cosmo.stan@yandex.com>
2024-11-30 16:46:18 +0300

    chore: add config for PostgreSQL production db

--- knexfile.js
+++ knexfile.js
@@ -28,9 +28,14 @@ export const test = {
 };
 
 export const production = {
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DB,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    ssl: true,
   },
   useNullAsDefault: true,
   migrations,
